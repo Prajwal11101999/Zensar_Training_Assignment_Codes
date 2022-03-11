74c7cadbcf9c0b2b40428fb6519bd84f0de361a7
Prajwal11101999 <prajwal.borawake85@gmail.com>
2022-03-11 15:44:40 +0530

    ASP WebApplication Assignment 1

---
+++ Web Application Assignment 1/ASPWebAppAssignment1/ASPWebAppAssignment1/ASPAssignment_1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPWebAppAssignment1
+{
+    public partial class ASPAssignment_1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+        protected void Submit_click(object sender, EventArgs e)
+        {
+            Response.Write("Your Registration is Successful.");
+            
+        }
+    }
+}
